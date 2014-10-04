bd4afd95a3b592cae96b73d4bbfb10072f42dcad
Peter Law <PeterJCLaw@gmail.com>
2014-10-04 23:18:06 +0100

    Add our LDAP connection
    This is a wrapper around the existing srusers repo.
    
    Change-Id: I6fde8c79dd56e110e965f4543654d885eaab3327

---
+++ fritter/ldap_connector.py
@@ -0,0 +1,58 @@
+
+from . import srusers
+
+class UnknownGroup(Exception):
+    """An exception to raise when we are asked about a group that isn't
+    in the database.
+    """
+    def __init__(self, group_name):
+        super(UnknownGroup, self).__init__(
+            "Unknown group '{0}'.".format(group_name)
+        )
+
+class InvalidGroup(Exception):
+    """An exception to raise when we are asked about a group that isn't
+    in the list of valid groups.
+    """
+    def __init__(self, group_name):
+        super(InvalidGroup, self).__init__(
+            "Group '{0}' is not allowed.".format(group_name)
+        )
+
+class LDAPGroupConnector(object):
+    def __init__(self, valid_groups):
+        """Create a new connector.
+
+        Parameters
+        ----------
+        valid_groups : list of str
+            A list of names which are valid for this instance to lookup
+            information about. Will raise ``UnknownGroup`` for any which
+            don't exist in the underlying database.
+        """
+        map(self._get_group, valid_groups)
+        self._valid_groups = valid_groups
+
+    def _get_group(self, group_name):
+        g = srusers.group(group_name)
+        if not g.in_db:
+            raise UnknownGroup(group_name)
+        return g
+
+    def _check_valid(self, group_name):
+        if not group_name in self._valid_groups:
+            raise InvalidGroup(group_name)
+
+    def describe(self, group_name):
+        self._check_valid(group_name)
+        g = self._get_group(group_name)
+        return g.desc or group_name
+
+    def get_emails(self, group_name):
+        self._check_valid(group_name)
+        g = self._get_group(group_name)
+        emails = []
+        for uid in g.members:
+            u = srusers.user(uid)
+            emails.append(u.email)
+        return emails
---
+++ test/test_ldap_connector.py
@@ -0,0 +1,105 @@
+
+import mock
+
+from fritter.ldap_connector import LDAPGroupConnector, InvalidGroup, UnknownGroup
+
+def get_mock_srusers():
+    return mock.patch('fritter.ldap_connector.srusers')
+
+def get_mock_group_ctor(in_db, desc = None, members = []):
+    mock_group = mock.Mock()
+    mock_group.in_db = in_db
+    mock_group.desc = desc
+    mock_group.members = members
+    mock_group_ctor = mock.Mock(return_value = mock_group)
+    return mock_group_ctor
+
+def get_mock_user_ctor(email):
+    mock_user = mock.Mock()
+    mock_user.email = email
+    mock_user_ctor = mock.Mock(return_value = mock_user)
+    return mock_user_ctor
+
+def test_valid_group_missing():
+    with get_mock_srusers() as mock_srusers:
+        mock_srusers.group = get_mock_group_ctor(in_db = False)
+
+        threw = False
+        group_name = 'my-missing-group'
+        try:
+            c = LDAPGroupConnector([group_name])
+        except UnknownGroup as ug:
+            msg = ug.message
+            assert group_name in msg, "Should include the group name in the message"
+            threw = True
+
+        assert threw, "Should have errored about the missing group"
+
+def test_valid_group():
+    with get_mock_srusers() as mock_srusers:
+        mock_srusers.group = get_mock_group_ctor(in_db = True)
+
+        c = LDAPGroupConnector(['found'])
+
+def test_describe_invalid_group():
+    with get_mock_srusers() as mock_srusers:
+        mock_srusers.group = get_mock_group_ctor(in_db = True)
+
+        c = LDAPGroupConnector(['valid'])
+
+        threw = False
+        group_name = 'other'
+        try:
+            c.describe(group_name)
+        except InvalidGroup as ig:
+            msg = ig.message
+            assert group_name in msg, "Should include the group name in the message"
+            threw = True
+
+        assert threw, "Should have errored about the invalid group"
+
+def test_describe_valid_group():
+    with get_mock_srusers() as mock_srusers:
+        desc = 'bacon'
+        mock_srusers.group = get_mock_group_ctor(in_db = True, desc = desc)
+
+        name = 'valid'
+        c = LDAPGroupConnector([name])
+
+        actual = c.describe(name)
+        assert desc == actual, "Wrong description returned"
+
+def test_describe_valid_group_no_description():
+    with get_mock_srusers() as mock_srusers:
+        mock_srusers.group = get_mock_group_ctor(in_db = True)
+
+        name = 'valid'
+        c = LDAPGroupConnector([name])
+
+        actual = c.describe(name)
+        assert name == actual, "Should fall back to the name if the description is empty"
+
+def test_emails_empty_group():
+    with get_mock_srusers() as mock_srusers:
+        mock_srusers.group = get_mock_group_ctor(in_db = True)
+
+        name = 'valid'
+        c = LDAPGroupConnector([name])
+
+        actual = c.get_emails(name)
+        assert [] == actual, "Should return empty list for empty group"
+
+def test_describe_valid_group_no_description():
+    with get_mock_srusers() as mock_srusers:
+        member_name = 'my-member-uid'
+        mock_srusers.group = get_mock_group_ctor(in_db = True, members = [member_name])
+        email = 'tim@example.com'
+        mock_srusers.user = get_mock_user_ctor(email)
+
+        name = 'valid'
+        c = LDAPGroupConnector([name])
+
+        actual = c.get_emails(name)
+        expected = [email]
+        assert expected == actual, "Should return a list of emails"
+        mock_srusers.user.assert_called_once_with(member_name)
